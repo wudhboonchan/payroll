f593b4eebd01f6e5602d1e203c9f1abd864d0aa6
Wudh Boonchan <wudh.boonchan@gmail.com>
2026-05-23 01:42:49 +0200

    Test LIFF

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+console.log("LIFF ID:", import.meta.env.VITE_LIFF_ID);
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
